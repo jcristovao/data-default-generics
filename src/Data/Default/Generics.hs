ed09e1439985937ae6ab1564385e5335b828a629
João Cristóvão <jmacristovao@gmail.com>
2014-02-17 13:17:28 +0000

    Added several instances. Refactored tests.

--- src/Data/Default/Generics.hs
+++ src/Data/Default/Generics.hs
@@ -33,7 +33,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 {-# LANGUAGE FlexibleContexts     #-}
 {-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE FlexibleInstances    #-}
-module Data.Default (
+module Data.Default.Generics (
 -- | This module defines a class for types with a default value. Instances are
 -- provided for '()', 'S.Set', 'M.Map', 'Int', 'Integer', 'Float', 'Double',
 -- and many others (see below).
@@ -42,6 +42,8 @@ module Data.Default (
 
 import Data.Int
 import Data.Word
+import Data.Fixed
+import Foreign.C.Types
 import Data.Monoid
 import Data.Ratio
 import Data.Complex
@@ -52,8 +54,25 @@ import Data.IntSet (IntSet)
 import Data.Sequence (Seq)
 import Data.Tree (Tree(..))
 import Data.DList (DList)
+import qualified Data.Text            as T
+import qualified Data.Text.Lazy       as TL
+import qualified Data.ByteString      as BS
+import qualified Data.ByteString.Lazy as BSL
+import qualified Data.HashMap.Lazy    as HSL
+import qualified Data.Vector          as V
+import qualified Data.Vector.Storable as VS
+import qualified Data.Vector.Unboxed  as VU
+import qualified Data.Vector.Primitive as VP
+-- Strict only in functions, data structure is the same
+{-import qualified Data.HashMap.Strict  as HSS-}
+import qualified Data.HashSet         as HSet
 import System.Locale
+import System.IO
 import GHC.Generics
+
+import Data.Time.Calendar
+import Data.Time.Clock
+import Data.Time.LocalTime
 
 -- | A class for types with a default value.
 class Default a where
@@ -62,12 +81,7 @@ class Default a where
     default def :: (Generic a, GDefault (Rep a)) => a
     def = to gDef
 
-instance Default (S.Set v) where def = S.empty
-instance Default (M.Map k v) where def = M.empty
-instance Default (IntMap v) where def = mempty
-instance Default IntSet where def = mempty
-instance Default (Seq a) where def = mempty
-instance (Default a) => Default (Tree a) where def = Node def def
+instance Default Bool where def = False
 
 instance Default Int where def = 0
 instance Default Int8 where def = 0
@@ -85,24 +99,57 @@ instance Default Double where def = 0
 instance (Integral a) => Default (Ratio a) where def = 0
 instance (Default a, RealFloat a) => Default (Complex a) where def = def :+ def
 
-instance (Default r) => Default (e -> r) where def = const def
-instance (Default a) => Default (IO a) where def = return def
+-- C Interface
+instance Default CChar    where def = 0
+instance Default CSChar   where def = 0
+instance Default CUChar   where def = 0
+instance Default CShort   where def = 0
+instance Default CUShort  where def = 0
+instance Default CInt     where def = 0
+instance Default CUInt    where def = 0
+instance Default CLong    where def = 0
+instance Default CULong   where def = 0
+-- instance Default CPtrdiff where def = 0 -- this just seems wrong
+instance Default CSize    where def = 0
+instance Default CWchar   where def = 0
+instance Default CLLong   where def = 0
+instance Default CULLong  where def = 0
+instance Default CFloat   where def = 0
+instance Default CDouble  where def = 0
 
+instance Default (Fixed E0) where def = 0
+instance Default (Fixed E1) where def = 0
+instance Default (Fixed E2) where def = 0
+instance Default (Fixed E3) where def = 0
+instance Default (Fixed E6) where def = 0
+instance Default (Fixed E9) where def = 0
+instance Default (Fixed E12)where def = 0
+
+
+-- System.IO
+-- should these be here?
+instance Default IOMode       where def = ReadMode
+instance Default TextEncoding where def = utf8
+instance Default Newline      where def = LF
+instance Default NewlineMode  where def = universalNewlineMode
+
+-- maybe, either, function, IO
 instance Default (Maybe a) where def = Nothing
+instance (Default a) => Default (Either a b)  where def = Left def
+instance (Default r) => Default (e -> r)      where def = const def
+instance (Default a) => Default (IO a)        where def = return def
 
+-- monoids
 instance Default () where def = mempty
 instance Default [a] where def = mempty
 instance Default Ordering where def = mempty
 instance Default Any where def = mempty
 instance Default All where def = mempty
-instance Default (Last a) where def = mempty
 instance Default (First a) where def = mempty
+instance Default (Last a) where def = mempty
 instance (Num a) => Default (Sum a) where def = mempty
 instance (Num a) => Default (Product a) where def = mempty
 instance Default (Endo a) where def = mempty
-
-instance Default (DList a) where def = mempty
-
 instance (Default a) => Default (Dual a) where def = Dual def
 instance (Default a, Default b) => Default (a, b) where def = (def, def)
 instance (Default a, Default b, Default c) => Default (a, b, c) where def = (def, def, def)
@@ -111,23 +158,66 @@ instance (Default a, Default b, Default c, Default d, Default e) => Default (a, 
 instance (Default a, Default b, Default c, Default d, Default e, Default f) => Default (a, b, c, d, e, f) where def = (def, def, def, def, def, def)
 instance (Default a, Default b, Default c, Default d, Default e, Default f, Default g) => Default (a, b, c, d, e, f, g) where def = (def, def, def, def, def, def, def)
 
+-- containers
+instance Default (S.Set v)    where def = S.empty
+instance Default (M.Map k v)  where def = M.empty
+instance Default (IntMap v)   where def = mempty
+instance Default IntSet       where def = mempty
+instance Default (Seq a)      where def = mempty
+instance (Default a) => Default (Tree a) where def = Node def def
+
+-- unordered-containers
+instance Default (HSL.HashMap k v) where def = HSL.empty
+instance Default (HSet.HashSet v)  where def = HSet.empty
+
+-- difference lists
+instance Default (DList a)    where def = mempty
+
+-- vectors
+instance Default (V.Vector v)        where def = V.empty
+instance (VS.Storable v) => Default (VS.Vector v) where def = VS.empty
+instance (VU.Unbox    v) => Default (VU.Vector v) where def = VU.empty
+instance (VP.Prim     v) => Default (VP.Vector v) where def = VP.empty
+
+
+instance Default T.Text   where def = T.empty
+instance Default TL.Text  where def = TL.empty
+instance Default BS.ByteString  where def = BS.empty
+instance Default BSL.ByteString where def = BSL.empty
+
+
+
+
 instance Default TimeLocale where def = defaultTimeLocale
+
+-- Data.Time.Calendar
+instance Default Day             where def = ModifiedJulianDay 0
+-- Data.Time.Clock
+instance Default UniversalTime   where def = ModJulianDate 0
+instance Default DiffTime        where def = secondsToDiffTime 0
+instance Default UTCTime         where def = UTCTime def def
+instance Default NominalDiffTime where def = diffUTCTime def def
+-- Data.Time.LocalTime
+instance Default TimeZone        where def = utc
+instance Default TimeOfDay       where def = midnight
+instance Default LocalTime       where def = utcToLocalTime utc def
+instance Default ZonedTime       where def = utcToZonedTime utc def
 
 -- Generic Default for default implementation
 class GDefault f where
     gDef :: f a
-    
+
 instance GDefault U1 where
     gDef = U1
 
 instance (Datatype d, GDefault a) => GDefault (D1 d a) where
-   gDef = M1 gDef 
+   gDef = M1 gDef
 
 instance (Constructor c, GDefault a) => GDefault (C1 c a) where
-   gDef = M1 gDef 
+   gDef = M1 gDef
 
 instance (Selector s, GDefault a) => GDefault (S1 s a) where
-   gDef = M1 gDef 
+   gDef = M1 gDef
 
 instance (Default a) => GDefault (K1 i a) where
    gDef = K1 def
---
+++ test/DataDefaultGenericsSpec.hs
@@ -0,0 +1,150 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module DataDefaultGenericsSpec where
+
+import Data.Int
+import Data.Word
+import Data.Fixed
+import Foreign.C.Types
+import Data.Monoid
+import Data.Ratio
+import Data.Complex
+import qualified Data.Set      as S
+import qualified Data.Map      as M
+import qualified Data.IntMap   as IM
+import qualified Data.IntSet   as IS
+import qualified Data.Sequence as Seq
+import qualified Data.Tree     as Tree
+import qualified Data.DList    as DList
+import qualified Data.Text            as T
+import qualified Data.Text.Lazy       as TL
+import qualified Data.ByteString      as BS
+import qualified Data.ByteString.Lazy as BSL
+import qualified Data.HashMap.Lazy    as HSL
+import qualified Data.HashSet         as HSet
+import qualified Data.Vector          as V
+import qualified Data.Vector.Storable as VS
+import qualified Data.Vector.Unboxed  as VU
+import qualified Data.Vector.Primitive as VP
+import System.Locale
+import System.IO
+import GHC.Generics
+
+import Data.Time.Calendar
+import Data.Time.Clock
+import Data.Time.LocalTime
+
+import Data.Default.Generics
+
+import Test.Hspec
+
+
+{-# ANN spec ("HLint: ignore Too strict if"::String) #-}
+spec :: Spec
+spec = describe "DataDefaultGenerics" $ do
+  -- Boolean
+  it "Bool"     $ (def :: Bool)               `shouldBe` False
+
+  -- Int
+  it "Int"      $ (def :: Int)            `shouldBe` 0
+  it "Int8"     $ (def :: Int)            `shouldBe` 0
+  it "Int16"    $ (def :: Int)            `shouldBe` 0
+  it "Int32"    $ (def :: Int)            `shouldBe` 0
+  it "Int64"    $ (def :: Int)            `shouldBe` 0
+  it "Word"     $ (def :: Int)            `shouldBe` 0
+  it "Word8"    $ (def :: Int)            `shouldBe` 0
+  it "Word16"   $ (def :: Int)            `shouldBe` 0
+  it "Word32"   $ (def :: Int)            `shouldBe` 0
+  it "Word64"   $ (def :: Int)            `shouldBe` 0
+  it "Integer"  $ (def :: Int)            `shouldBe` 0
+  it "Float"    $ (def :: Float)          `shouldBe` 0
+  it "Double"   $ (def :: Double)         `shouldBe` 0
+  it "Ratio a"  $ (def :: Ratio Int)      `shouldBe` 0
+  it "Complex a"$ (def :: Complex Double) `shouldBe` 0 :+ 0
+
+  -- C Interface
+  it "CChar"    $ (def :: CChar)    `shouldBe` 0
+  it "CSChar"   $ (def :: CSChar)   `shouldBe` 0
+  it "CUChar"   $ (def :: CUChar)   `shouldBe` 0
+  it "CShort"   $ (def :: CShort)   `shouldBe` 0
+  it "CUShort"  $ (def :: CUShort)  `shouldBe` 0
+  it "CInt"     $ (def :: CInt)     `shouldBe` 0
+  it "CUInt"    $ (def :: CUInt)    `shouldBe` 0
+  it "CLong"    $ (def :: CLong)    `shouldBe` 0
+  it "CULong"   $ (def :: CULong)   `shouldBe` 0
+  it "CSize"    $ (def :: CSize)    `shouldBe` 0
+  it "CWchar"   $ (def :: CWchar)   `shouldBe` 0
+  it "CLLong"   $ (def :: CLLong)   `shouldBe` 0
+  it "CULLong"  $ (def :: CULLong)  `shouldBe` 0
+  it "CFloat"   $ (def :: CFloat)   `shouldBe` 0
+  it "CDouble"  $ (def :: CDouble)  `shouldBe` 0
+
+  -- Fixed
+  it "Fixed E0" $ (def :: Fixed E0) `shouldBe` 0
+  it "Fixed E1" $ (def :: Fixed E1) `shouldBe` 0
+  it "Fixed E2" $ (def :: Fixed E2) `shouldBe` 0
+  it "Fixed E3" $ (def :: Fixed E3) `shouldBe` 0
+  it "Fixed E6" $ (def :: Fixed E6) `shouldBe` 0
+  it "Fixed E9" $ (def :: Fixed E9) `shouldBe` 0
+  it "Fixed E12"$ (def :: Fixed E12)`shouldBe` 0
+
+  -- System.IO
+  it "IOMode"   $ (def :: IOMode)   `shouldBe` ReadMode
+  it "Newline"  $ (def :: Newline)  `shouldBe` LF
+  it "NewlineMode" $ (def :: NewlineMode) `shouldBe` universalNewlineMode
+
+  -- maybe, either, function, IO
+  it "Maybe a"  $ (def :: Maybe Int) `shouldBe` Nothing
+  it "Either a b"$(def :: Either Int String) `shouldBe` Left 0
+  it "(->) r"   $ (def :: Int -> String) 0 `shouldBe` ""
+  it "IO a"     $ (def :: IO Int) `shouldReturn` 0
+
+  -- monoids
+  -- could test for mempty, but this is more educational
+  it "()"       $ (def :: ())                 `shouldBe` ()
+  it "[a]"      $ (def :: [Int])              `shouldBe` []
+  it "Ordering" $ (def :: Ordering)           `shouldBe` EQ
+  it "Any"      $ (def :: Any)                `shouldBe` Any False
+  it "All"      $ (def :: All)                `shouldBe` All True
+  it "First a"  $ (def :: First (Maybe Int))  `shouldBe` First Nothing
+  it "Last a"   $ (def :: Last (Maybe Int))   `shouldBe` Last Nothing
+  it "Sum a"    $ (def :: Sum Float)          `shouldBe` Sum 0
+  it "Product a"$ (def :: Product Int)        `shouldBe` Product 1
+  it "Endo a"   $ appEndo(def :: Endo Int) 1  `shouldBe` 1
+  it "Dual a"   $ getDual (def :: Dual Int)   `shouldBe` 0
+  it "()"       $ (def :: (Int,String))       `shouldBe` (0,"")
+
+  -- containers
+  it "Set v"    $ (def :: S.Set Int)         `shouldBe` S.empty
+  it "Map k v"  $ (def :: M.Map Int Int)     `shouldBe` M.empty
+  it "IntMap v" $ (def :: IM.IntMap String)  `shouldBe` IM.empty
+  it "IntSet"   $ (def :: IS.IntSet)         `shouldBe` IS.empty
+  it "Seq v"    $ (def :: Seq.Seq String)    `shouldBe` Seq.empty
+  it "Tree v"   $ (def :: Tree.Tree Int)     `shouldBe` Tree.Node def []
+
+  -- unordered-containers
+  it "HashMap k v" $ (def :: HSL.HashMap Int String) `shouldBe` HSL.empty
+  it "HashSet v"   $ (def :: HSet.HashSet String)    `shouldBe` HSet.empty
+
+  -- difference lists
+  it "DList a"  $ (def :: DList.DList String) `shouldBe` DList.empty
+
+  -- vectors
+  it "Vector v"            $ (def :: V.Vector String)  `shouldBe` V.empty
+  it "(Storable) Vector v" $ (def :: VS.Vector Char)   `shouldBe` VS.empty
+  it "(Unbox) Vector v"    $ (def :: VU.Vector Char)   `shouldBe` VU.empty
+  it "(Prim)  Vector v"    $ (def :: VP.Vector Char)   `shouldBe` VP.empty
+
+  -- Text & ByteString
+  it "Text"            $ (def :: T.Text)         `shouldBe` ""
+  it "Lazy Text"       $ (def :: TL.Text)        `shouldBe` ""
+  it "ByteString"      $ (def :: BS.ByteString)  `shouldBe` ""
+  it "Lazy ByteString" $ (def :: BSL.ByteString) `shouldBe` ""
+
+  -- Time
+  it "Day"             $ (def :: Day)           `shouldBe` ModifiedJulianDay 0
+  {-it "UniversalTime"   $ (def :: UniversalTime) `shouldBe` ModJulianDate 0-}
+  it "DiffTime"        $ (def :: DiffTime)      `shouldBe` secondsToDiffTime 0
+  it "UTCTime"         $ (def :: UTCTime)       `shouldBe` UTCTime def 0
